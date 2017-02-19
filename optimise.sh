dac1115b2a63dd41082d0aedcb6ed63a0dbfc754
Paul Hayes <fofr@users.noreply.github.com>
2017-02-19 23:40:42 +0000

    Use JPEGMini for much better image sizes
    Reduces previously optimised images by ~60-80%!

--- optimise.sh
+++ optimise.sh
@@ -1,8 +1,8 @@
 # 1720, 860, 388
-imageOptim -d 1720/
-imageOptim -d 860/
-#imageOptim -d 435/
-#imageOptim -d 380/
-imageOptim -d square/380/
-imageOptim -d watermarked/1720/
-imageOptim -d watermarked/435/
+imageOptim -j -d 1720/
+imageOptim -j -d 860/
+imageOptim -j -d 435/
+imageOptim -j -d 380/
+imageOptim -j -d square/380/
+imageOptim -j -d watermarked/1720/
+imageOptim -j -d watermarked/435/
